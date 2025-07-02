b7589b33324c62970c02c9d981149ead3fd41a0d
Ravikant tiwari <115395652+ravikantt45@users.noreply.github.com>
2025-07-02 11:15:37 +0530

    Add files via upload

---
+++ Router/src/App.js
@@ -0,0 +1,86 @@
+import Layout from "./Layout";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2025 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2025 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2025 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2025 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+    },
+  ]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Layout search={search} setSearch={setSearch} />}
+        >
+          <Route index element={<Home posts={searchResults} />} />
+          <Route path="post">
+            <Route
+              index
+              element={
+                <NewPost
+                  posts={posts}
+                  setPosts={setPosts}
+                  postTitle={postTitle}
+                  setPostTitle={setPostTitle}
+                  postBody={postBody}
+                  setPostBody={setPostBody}
+                />
+              }
+            />
+            <Route
+              path=":id"
+              element={<PostPage posts={posts} setPosts={setPosts} />}
+            />
+          </Route>
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<Missing />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Router/src/NewPost.js
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { format } from "date-fns";
+// npm install date-fns
+
+const NewPost = ({
+  posts,
+  setPosts,
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+}) => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle("");
+    setPostBody("");
+    navigate("/");
+  };
+  return (
+    <main className="NewPost">
+      <h2>New Post</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          id="postTitle"
+          type="text"
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <label htmlFor="postBody">Post:</label>
+        <textarea
+          id="postBody"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+};
+
+export default NewPost;
